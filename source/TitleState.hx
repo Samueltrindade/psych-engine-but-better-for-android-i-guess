ad9b943c6d3e80149ed74e7707dd07c2d28c83d8
Samueltrindade <90457866+Samueltrindade@users.noreply.github.com>
2022-07-14 16:14:39 -0300

    Add files via upload

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -87,7 +87,7 @@ class TitleState extends MusicBeatState
 	public static var updateVersion:String = '';
 
 	override public function create():Void
-  #if android || FlxG.android.justReleased.BACK #end
+  #if android || FlxG.android.justReleased.BACK; #end
 	{
 		Paths.clearStoredMemory();
 		Paths.clearUnusedMemory();
